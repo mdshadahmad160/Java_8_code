6d1dd3e3d38b3e67e4d769a460a9f132424fd461
Md Shad Ahmad <136076679+mdshadahmad160@users.noreply.github.com>
2025-04-24 16:06:41 +0530

    Reverse String is working fine

---
+++ com/nc/practice/ReverseString.java
@@ -0,0 +1,16 @@
+package com.nc.practice;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        String rev = "";
+
+        for (int i=str.length()-1;i>=0;i--){
+            rev = rev + str.charAt(i);
+        }
+        System.out.println("Original String: " + str);
+        System.out.println("Reversed String: " + rev);
+
+
+    }
+}
