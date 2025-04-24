ee88adb722626e2369a39256e1b133bb46787bd9
Md Shad Ahmad <136076679+mdshadahmad160@users.noreply.github.com>
2025-04-24 15:56:16 +0530

    new changes

---
+++ com/nc/practice/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.nc.practice;
+
+/**
+ * @author Shad Ahmad
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
